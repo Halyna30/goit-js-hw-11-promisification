ba358d52070af0de3e73daa992eee85a376fb37d
Halyna Pryn <prynhalyna@gmail.com>
2020-12-20 17:01:56 +0100

    Fix 20.12

--- index.js
+++ index.js
@@ -1,98 +1,16 @@
 // ============== Task 1 ==================
 
-// const delay = (ms) => {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve(ms);
-//     }, ms);
-//   });
-// };
-
-// const logger = (time) => console.log(`Resolved after ${time}ms`);
-
-// // Вызовы функции для проверки
-// delay(2000).then(logger); // Resolved after 2000ms
-// delay(1000).then(logger); // Resolved after 1000ms
-// delay(1500).then(logger); // Resolved after 1500ms
-
-// ============== Task 2 ==================
-
-// const users = [
-//   { name: "Mango", active: true },
-//   { name: "Poly", active: false },
-//   { name: "Ajax", active: true },
-//   { name: "Lux", active: false },
-// ];
-
-// const toggleUserState = (allUsers, userName) => {
-//   return new Promise((resolve) => {
-//     const updatedUsers = allUsers.map((user) =>
-//       user.name === userName ? { ...user, active: !user.active } : user
-//     );
-//     resolve(updatedUsers);
-//   });
-// };
-
-// const logger = (updatedUsers) => console.table(updatedUsers);
-
-// /*
-//  * Сейчас работает так
-//  */
-// // toggleUserState(users, "Mango", logger);
-// // toggleUserState(users, "Lux", logger);
-
-// /*
-//  * Должно работать так
-//  */
-// toggleUserState(users, "Mango").then(logger);
-// toggleUserState(users, "Lux").then(logger);
-
-// ============== Task 3 ==================
-
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-const makeTransaction = (transaction) => {
-  return new Promise((res, rej) => {
-    const delay = randomIntegerFromInterval(200, 500);
-
+const delay = (ms) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      const canProcess = Math.random() > 0.3;
-
-      if (canProcess) {
-        res(`Transaction ${transaction.id} processed in ${delay}ms`);
-      } else {
-        rej(
-          `Error processing transaction ${transaction.id}. Please try again later.`
-        );
-      }
-    }, delay);
+      resolve(ms);
+    }, ms);
   });
 };
 
-const logSuccess = (result) => {
-  console.log(result);
-};
+const logger = (time) => console.log(`Resolved after ${time}ms`);
 
-const logError = (result) => {
-  console.log(result);
-};
-
-/*
- * Работает так
- */
-// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
-// /*
-//  * Должно работать так
-//  */
-makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
-
-makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
-
-makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
-
-makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
+// // Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
